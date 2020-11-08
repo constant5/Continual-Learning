c1811c264453ac843385d74453adf6a5dee53bcb
constant5 <crmarksv@gmail.com>
2020-11-08 14:47:27 -0600

    added file_writer method

--- scripts/BZ2Reader.py
+++ scripts/BZ2Reader.py
@@ -1,7 +1,8 @@
 # Module for opening reddit bz2 files
 
 import json
-import bz2 
+import bz2
+import os
 
 class bz2reader():
 
@@ -55,7 +56,47 @@ class bz2reader():
                     else:
                         data[p['subreddit']] = [{k:p[k] for k in self.keys  if k!='subreddit'}]
         return data
+# # TODO: 
 
+    def file_writer(self, dest = "data", p_min=1000, batch_save=1000):
+        # make dir to hold txt files
+        if not os.path.exists(os.path.join(dest, 'subreddits','')):
+            os.mkdir(os.path.join(dest, 'subreddits'))
+        
+        # create temporary data structure to batch save data
+        data = {}
+        for post in self.select_keys():
+            if post['body'] != 'deleted' and len(post['body'].split(' ')) >= 4:
+                try:
+                    # data[p['subreddit']] = data[p['subreddit']].append([{k:p[k] for k in self.keys() if k!='subreddit'}])
+                    if len(self.keys) == 2:
+                        data[post['subreddit']].append(post['body'])
+                    else:
+                        data[post['subreddit']].append({k:post[k] for k in self.keys  if k!='subreddit'})
+                    # print('appending')
+                except:
+                    # print('creating new')
+                    if len(self.keys) ==2:
+                        data[post['subreddit']] = [post['body']]
+                    else:
+                        data[post['subreddit']] = [{k:post[k] for k in self.keys  if k!='subreddit'}]
+            
+                
+                data_path = os.path.join(dest,'subreddits',  post['subreddit']+'.txt')
+                
+                # for data that is being saved save batch
+                if os.path.exists(data_path) and len(data[post['subreddit']]) >= batch_save:
+                    with open(data_path, 'a', encoding='utf-8') as f:
+                            f.writelines([l.replace('\n',' ') + '\n' for l in data[post['subreddit']]])
+                    data[post['subreddit']] = []
+
+                 # if data structure has more the p_min post for a given subreddit then create file
+                if len(data[post['subreddit']]) >= p_min:
+                    print(f'Initializing file for {post["subreddit"]}')
+                    with open(data_path, 'w', encoding='utf-8') as f:
+                        f.writelines([l.replace('\n',' ') + '\n' for l in data[post['subreddit']]])
+                    # reset the data structure list for processed batch
+                    data[post['subreddit']] = []
 
     
     def __del__(self):
